dcddbdafc259edb30380cb11ba8ffe85a956ed8c
utkxrsh13 <utkarsht0813@gmail.com>
2024-11-21 15:35:45 +0530

    applicant

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -1,2 +1,10 @@
-// Highlights
-rftghjuklgit 
+import express from "express"
+import isAuthenticated from "../middlewares/isAuthenticated.js"
+import { applyJob, getApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js"
+
+const router = express.Router();
+
+router.route("/apply/:id").get(isAuthenticated, applyJob);
+router.route("/get").get(isAuthenticated, getAppliedJobs);
+router.route("/:id/applicant").get(isAuthenticated, getApplicants);
+router.route("/status/:id/update").post(isAuthenticated, updateStatus);
